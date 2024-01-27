1d8df6ba916315c8b9a0af84839b1cdc67d7aada
Ella <78714820+MoonGyu1@users.noreply.github.com>
2024-01-27 18:13:56 +0900

    Title: 최고의 33위치, Time: 155ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240127/최고의 33위치/best-place-of-33.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class Main {
+    public static final int MAX_N = 20;
+
+    public static int n;
+    public static int[][] grid = new int[MAX_N][MAX_N];
+
+    public static int getNumOfGold(int rs, int cs, int re, int ce) {
+        int numOfGold = 0;
+
+        for(int row = rs; row <= re; row++) {
+            for(int col = cs; col <= ce; col++){
+                numOfGold += grid[row][col];
+            }
+        }
+
+        return numOfGold;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int maxGold = 0;
+
+        n = sc.nextInt();
+        for(int row = 0; row < n; row++)
+            for(int col = 0; col < n; col++)
+                grid[row][col] = sc.nextInt();
+        
+        for(int row = 0; row < n; row++){
+            for(int col = 0; col < n; col++){
+                if(row + 2 >= n || col + 2 >= n)
+                    continue;
+                
+                int numOfGold = getNumOfGold(row, col, row + 2, col + 2);
+
+                maxGold = Math.max(maxGold, numOfGold);
+            }
+        }
+
+        System.out.println(maxGold);
+    }
+}
