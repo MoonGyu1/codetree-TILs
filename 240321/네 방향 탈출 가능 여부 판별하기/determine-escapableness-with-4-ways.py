b56ec0ec047324028ea305d57d3ca9db52234ca1
Ella <78714820+MoonGyu1@users.noreply.github.com>
2024-03-21 16:02:57 +0900

    Title: 네 방향 탈출 가능 여부 판별하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240321/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.py
+++ 240321/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.py
@@ -1,55 +1,39 @@
 from collections import deque
 
-# 변수 선언 및 입력
-n, m = tuple(map(int, input().split()))
+def can_go(x, y):
+    if not in_range(x, y):
+        return False
+    if not visited[x][y] and graph[x][y] != 0:
+        return True
+    return False
 
-a = [
-    list(map(int, input().split()))
-    for _ in range(n)
-]
+def in_range(x, y):
+    if 0 <= x < n and 0 <= y < m:
+        return True
+    return False
 
-visited = [
-    [False for _ in range(m)]
-    for _ in range(n)
-]
+n, m = map(int, input().split())
+graph = [[] for _ in range(n)]
+visited = [[False for _ in range(m)] for _ in range(m)]
+for i in range(n):
+    graph[i] = list(map(int, input().split()))
+    
+x, y = 0, 0
+dxs, dys = [-1, 1, 0, 0], [0, 0, -1, 1]
+success = 0
 
 q = deque()
+visited[x][y] = True
+q.append((x, y))
 
-# 주어진 위치가 격자를 벗어나는지 여부를 반환합니다.
-def in_range(x, y):
-    return 0 <= x and x < n and 0 <= y and y < m
+while q:
+    v = q.popleft()
+    for dx, dy in zip(dxs, dys):
+        next_x = v[0] + dx
+        next_y = v[1] + dy
+        if can_go(next_x, next_y):
+            visited[next_x][next_y] = True
+            q.append((next_x, next_y))
 
-
-# 주어진 위치로 이동할 수 있는지 여부를 확인합니다.
-def can_go(x, y):
-    return in_range(x, y) and a[x][y] and not visited[x][y]
-
-
-def bfs():
-    # queue에 남은 것이 없을때까지 반복합니다.
-    while q:
-        # queue에서 가장 먼저 들어온 원소를 뺍니다.
-        x, y = q.popleft()
-        
-        # queue에서 뺀 원소의 위치를 기준으로 4방향을 확인해봅니다.
-        dxs, dys = [0, 1, 0, -1], [1, 0, -1, 0]
-        for dx, dy in zip(dxs, dys):
-            new_x, new_y = x + dx, y + dy
-        
-            # 아직 방문한 적이 없으면서 갈 수 있는 곳이라면
-            # 새로 queue에 넣어주고 방문 여부를 표시해줍니다.
-            if can_go(new_x, new_y):
-                q.append((new_x, new_y))
-                visited[new_x][new_y] = True
-
-                
-# bfs를 이용해 최소 이동 횟수를 구합니다.
-# 시작점을 queue에 넣고 시작합니다.
-q.append((0, 0))
-visited[0][0] = True
-
-bfs()
-
-# 우측 하단을 방문한 적이 있는지 여부를 출력합니다.
-answer = 1 if visited[n - 1][m - 1] else 0
-print(answer)
+success = 1 if visited[n-1][m-1] else 0
+print(success)
