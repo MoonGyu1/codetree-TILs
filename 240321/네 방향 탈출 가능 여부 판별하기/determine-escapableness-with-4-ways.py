0d2612045922020f923712345d9e9a76cda2a936
Ella <78714820+MoonGyu1@users.noreply.github.com>
2024-03-21 16:10:36 +0900

    Title: 네 방향 탈출 가능 여부 판별하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240321/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.py
+++ 240321/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.py
@@ -1,33 +1,30 @@
 from collections import deque
 
+n, m = map(int, input().split())
+graph = [[] for _ in range(n)]
+visited = [[False for _ in range(m)] for _ in range(m)]
+q = deque()
+for i in range(n):
+    graph[i] = list(map(int, input().split()))
+
 def can_go(x, y):
     return in_range(x, y) and graph[x][y] != 0 and not visited[x][y]
 
 def in_range(x, y):
     return 0 <= x < n and 0 <= y < m
 
-n, m = map(int, input().split())
-graph = [[] for _ in range(n)]
-visited = [[False for _ in range(m)] for _ in range(m)]
-for i in range(n):
-    graph[i] = list(map(int, input().split()))
-    
-x, y = 0, 0
-dxs, dys = [-1, 1, 0, 0], [0, 0, -1, 1]
-success = 0
-
-q = deque()
-
 def bfs():
     while q:
         v = q.popleft()
+
+        dxs, dys = [-1, 1, 0, 0], [0, 0, -1, 1]
         for dx, dy in zip(dxs, dys):
             next_x = v[0] + dx
             next_y = v[1] + dy
             if can_go(next_x, next_y):
                 visited[next_x][next_y] = True
                 q.append((next_x, next_y))
-
+    
 visited[0][0] = True
 q.append((0, 0))
 
