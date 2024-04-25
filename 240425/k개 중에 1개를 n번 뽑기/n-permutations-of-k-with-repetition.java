67ee96be0d4dbe117bfdcc6336d827ee8d82725f
Ella <78714820+MoonGyu1@users.noreply.github.com>
2024-04-25 15:06:29 +0900

    Title: k개 중에 1개를 n번 뽑기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240425/k개 중에 1개를 n번 뽑기/n-permutations-of-k-with-repetition.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Main {
+    public static int k, n;
+    public static ArrayList<Integer> a = new ArrayList<Integer>();
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        k = sc.nextInt();
+        n = sc.nextInt();
+
+        chooseNum(1);   
+    }
+
+    public static void print() {
+        System.out.printf("%d %d\n", a.get(0), a.get(1));
+    }
+
+    public static void chooseNum(int currIdx) {
+        if(currIdx == n + 1){
+            print();
+            return;
+        }
+
+        for(int i = 1; i < k+1; i++){
+            a.add(i);
+            chooseNum(currIdx + 1);
+            a.remove(a.size() - 1);
+        }
+    }
+}
