b24efc0beebe8655327f1cd69c201649d9c7be0e
Ella <78714820+MoonGyu1@users.noreply.github.com>
2024-04-25 17:05:02 +0900

    Title: 강력한 폭발, Time: 213ms, Memory: 12MB, Status: Passed - CodeTree

--- 240425/강력한 폭발/strong-explosion.java
+++ 240425/강력한 폭발/strong-explosion.java
@@ -52,6 +52,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         n = sc.nextInt();
+        
         map = new int[n][n];
         for(int i=0; i<n; i++){
             for(int j=0; j<n; j++){
