3a20880bbd80a70bb4c37eb757b4b8586e268f61
gyu <78714820+MoonGyu1@users.noreply.github.com>
2024-10-07 23:20:12 +0900

    Title: 색깔 트리, Time: 5152ms, Memory: 220MB, Status: Passed - CodeTree

--- 241007/색깔 트리/color-tree.java
+++ 241007/색깔 트리/color-tree.java
@@ -6,7 +6,7 @@ public class Main {
 	static HashMap<Integer, Node> nodes = new HashMap<>();
 	
 	public static void main(String[] args) throws Exception {
-		// System.setIn(new FileInputStream("src/s202401_am_1/input2.txt"));
+		// System.setIn(new FileInputStream("src/s202401_am_1/input.txt"));
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
@@ -26,16 +26,16 @@ public class Main {
 					int max_depth = Integer.parseInt(st.nextToken());
 					
 					// 부모 노드의 maxDepth에 위배되는 경우 추가 X
-					if(p_id != -1) {
+					if(p_id != -1) { // 루트 노드가 아닌 경우 검
 						Node p = nodes.get(p_id);
+						int pCurrentDepth = 2;
 						
-						int pDepth = Math.max(p.currentDepth, 2);
-						while(pDepth <= p.maxDepth && p.pid != -1) {
+						while(pCurrentDepth <= p.maxDepth && p.pid != -1) {
 							p = nodes.get(p.pid);
-							pDepth = Math.max(p.currentDepth, pDepth + 1);
+							pCurrentDepth++;
 						}
 						
-						if(p.pid != -1 || p.maxDepth < pDepth) { // 위배
+						if(p.pid != -1 || p.maxDepth < pCurrentDepth) { // 위배
 							continue;
 						} else {
 							nodes.get(p_id).children.add(m_id);
@@ -51,62 +51,25 @@ public class Main {
 					node.pid = p_id;
 					node.color = color;
 					node.maxDepth = max_depth;
-					node.currentDepth = 1;
-					node.value.add(color);
-								
-					// 주의: id가 -1인 노드는 인스턴스 자체가 없음 -> 프로퍼티 호출 시 null 참조 에러 발생
-					if(p_id != -1) {
-						Node p = nodes.get(node.pid);
-						int currentDepth = 2;
-						while(p.pid != -1) {
-							p.currentDepth = Math.max(p.currentDepth, currentDepth);
-							p.value.add(color);
-							
-							p = nodes.get(p.pid);
-							currentDepth++;
-						}
-						p.currentDepth = Math.max(p.currentDepth, currentDepth);
-						p.value.add(color);
-					}
 			
 					nodes.put(m_id, node);
-					
-//					for(Map.Entry<Integer, Node> e : nodes.entrySet()) {
-//						System.out.println("id: " + e.getKey() + " node: " + e.getValue().pid);
-//					}
-					
+
 					break;
 				case "200": // 색깔 변경
 					int rootId = Integer.parseInt(st.nextToken());
 					int toBeColor = Integer.parseInt(st.nextToken());
-					
-					nodes.get(rootId).color = toBeColor;
-					nodes.get(rootId).value.clear();
-					nodes.get(rootId).value.add(toBeColor);
+					Node root = nodes.get(rootId);
+					root.color = toBeColor;
 					
 					// 주의: 새로운 인스턴스 할당해야 별도의 리스트로 관리 가능
-					Queue<Integer> children = new LinkedList<>(nodes.get(rootId).children);
+					Queue<Integer> children = new LinkedList<>(root.children);
 					while(!children.isEmpty()) {
 						Node child = nodes.get(children.poll());
 						child.color = toBeColor;
-						child.value.clear();
-						child.value.add(toBeColor);
+
 						for(int c : child.children) {
 							children.add(c);
 						}
-					}
-					
-					int pid = nodes.get(rootId).pid;
-					while(pid != -1) {
-						Node p = nodes.get(pid);
-						p.value.clear();
-						for(int c : p.children) {
-							for(int v : nodes.get(c).value) {
-								p.value.add(v);
-							}
-						}
-						p.value.add(p.color);
-						pid = p.pid;
 					}
 					
 					break;
@@ -134,26 +97,29 @@ public class Main {
 	}
 	
 	static int getValueByInOrder(int id) {
-		if(nodes.get(id).children.size() == 0) {
-//			HashSet<Integer> s = new HashSet<>();
-//			s.add(nodes.get(id).color);
-//			nodes.get(id).value = s;
+		Node node = nodes.get(id);
+		
+		if(node.children.size() == 0) {
+			HashSet<Integer> s = new HashSet<>();
+			s.add(node.color);
+			node.value = s;
+			
 			return 1;
 		}
 		
 		int value = 0;
-//		HashSet<Integer> s = new HashSet<>();
-		for(int c : nodes.get(id).children) {
+		HashSet<Integer> s = new HashSet<>();
+		for(int c : node.children) {
 			value += getValueByInOrder(c);
-//			for(int v : nodes.get(c).value) {
-//				s.add(v);
-//			}
-//			s.add(nodes.get(id).color);
-//			nodes.get(id).value = s;
+			for(int v : nodes.get(c).value) {
+				s.add(v);
+			}
 		}
+		s.add(node.color);
+		node.value = s;
 		
-//		return value + (int) Math.pow(s.size(), 2);
-		return value + (int) Math.pow(nodes.get(id).value.size(), 2);
+		return value + (int) Math.pow(s.size(), 2);
+//		return value + (int) Math.pow(nodes.get(id).value.size(), 2);
 	}
 
 }
@@ -166,8 +132,7 @@ class Node {
 	ArrayList<Integer> children = new ArrayList<>();
 	int color;
 	int maxDepth;
-	int currentDepth;
-	HashSet<Integer> value = new HashSet<>();
+	HashSet<Integer> value;
 }
 
 // 트리 만들기
