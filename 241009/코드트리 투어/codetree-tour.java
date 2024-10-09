c3761bee56bc298ff55601048733efde9fec6bb2
gyu <78714820+MoonGyu1@users.noreply.github.com>
2024-10-09 16:46:11 +0900

    Title: 코드트리 투어, Time: 3012ms, Memory: 56MB, Status: Time Limit Exceed - CodeTree

--- 241009/코드트리 투어/codetree-tour.java
+++ 241009/코드트리 투어/codetree-tour.java
@@ -2,7 +2,7 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-	static int MAX_R = 1001;
+	static int MAX_R = 1001; // 리스트 비교연산 시 오버플로우 주의 
 	static int[] dist;
 	static int n;
 	static int[][] weight;
@@ -16,18 +16,20 @@ public class Main {
 		int Q = Integer.parseInt(br.readLine());
 		int m;
 		
-		HashMap<Integer, int[]> travelInfo = new HashMap<>(); // id: revenue, dest, deleted (매출, 목적지, 삭제여부)
+		// id: revenue, dest, deleted (매출, 목적지, 삭제여부)
+		HashMap<Integer, int[]> travelInfo = new HashMap<>();
 		
 		// {cost, id}
 		PriorityQueue<int[]>travelList = new PriorityQueue<>((a, b) -> a[0] == b[0] ? a[1] - b[1] : b[0] - a[0]);
+		
 		int s = 0;
 			
 		while(Q --> 0) {			
 			st = new StringTokenizer(br.readLine());
 			String c = st.nextToken();
 //			System.out.println("q: " + Q + " c: " + c);
+			
 			switch(c) {
-				
 				case "100": // 건설
 					n = Integer.parseInt(st.nextToken());
 					m = Integer.parseInt(st.nextToken());
@@ -43,9 +45,7 @@ public class Main {
 						int v1 = Integer.parseInt(st.nextToken());
 						int v2 = Integer.parseInt(st.nextToken());
 						int w = Integer.parseInt(st.nextToken());
-						
-//						System.out.println(v1 +" " +v2 + " " + w);
-						
+												
 						weight[v1][v2] = Math.min(weight[v1][v2], w);
 						weight[v2][v1] = Math.min(weight[v2][v1], w);
 					}
@@ -54,17 +54,8 @@ public class Main {
 						weight[i][i] = 0;
 					}
 					
-//					for(int i=0; i<n; i++) {
-//						System.out.println(Arrays.toString(weight[i]));
-//					}
-					
 					getDist(s);
-					
-//					System.out.println("dist: " + Arrays.toString(dist));
-					
-					
-					
-					
+		
 					break;
 				case "200": // 상품 생성 
 					int id = Integer.parseInt(st.nextToken());
@@ -73,12 +64,7 @@ public class Main {
 					
 					travelInfo.put(id, new int[] {revenue, dest, 0});
 					
-//					if(dist[dest] == MAX_R) {
-//						continue;
-//					}
-					
 					int cost = revenue - dist[dest];
-//					if(cost < 0) continue;
 					
 					travelList.add(new int[] {cost, id});
 					
@@ -88,35 +74,22 @@ public class Main {
 					if(travelInfo.containsKey(id)) {
 						travelInfo.get(id)[2] = 1; // deleted
 					}
-//					
-//					int[] travel = new int[4];
-//					for(int[] t : travelList) {
-//						if(t[1] == id) {
-//							travel = t;
-//							break;
-//						}
-//					}
-//					
-//					travelList.remove(travel);
 					
 					break;
 				case "400": // 판매 
-//					for(int[] pp :travelList) { 
-//						System.out.println("cost: " + pp[0] + " id: " + pp[1]+ " deleted: " + (travelInfo.get(travelList.peek()[1])[2] == 1));
+//					while(!travelList.isEmpty() && travelInfo.get(travelList.peek()[1])[2] == 1) {
+//						travelList.poll();
 //					}
-					
-					while(!travelList.isEmpty() && travelInfo.get(travelList.peek()[1])[2] == 1) {
-						travelList.poll();
-					}
-					
-					if(travelList.isEmpty()) {				
-						System.out.println("-1");
-						break;
-					}
-					
-					
+//					
+//					if(travelList.isEmpty()) {				
+//						System.out.println("-1");
+//						break;
+//					}
+//					
+//					
 					ArrayList<int[]> tmpList = new ArrayList<>();
 					int num = -1;
+					
 					while(!travelList.isEmpty()) {
 						int[] prod = travelList.poll(); // cost, id
 						cost = prod[0];
@@ -133,7 +106,6 @@ public class Main {
 						}
 						
 					}
-					
 				
 					for(int[] t : tmpList) {
 						travelList.add(t);
@@ -143,50 +115,34 @@ public class Main {
 		
 					break;
 				case "500": // 출발지 변경
-					s = Integer.parseInt(st.nextToken());
+					int newS = Integer.parseInt(st.nextToken());
+					if(s == newS) continue;
 					
+					s = newS;
 					getDist(s);
 
 				
 					// 기존 판매상품 관리 (cost 재계산)
 //					
+					// 주의: 우선순위큐 내부 요소 직접 변경하는 경우 우선순위 반영 안 됨
 					tmpList = new ArrayList<>();
-					// 내부 요소 직접 변경하는 경우 우선순위 반영 안 됨 --> O
-	
-				
-					for(int[] t : travelList) {
-						int[] travel = travelInfo.get(t[1]);
-						t[0] = travel[0] - dist[travel[1]];
-					}
 					
 					while(!travelList.isEmpty()) {
-//						int[] travel = travelList.poll();
 						id = travelList.poll()[1];
-//						
-//						boolean deleted = travelInfo.get(id)[2] == 1;
-//						if(deleted) continue;
 						
 						int[] travel = travelInfo.get(id);
+						if(travel[2] == 1) continue;
+						
 						revenue = travel[0];
 						dest = travel[1];
 						cost = revenue - dist[dest];
 						
 						tmpList.add(new int[] {cost, id});
 					}
-////					
-////					travelList.clear();
-////					travelList = new PriorityQueue<>((a, b) -> a[0] == b[0] ? a[1] - b[1] : b[0] - a[0]);
+
 					for(int[] t : tmpList) {
 						travelList.add(t);
 					}
-					
-//					System.out.println("우선순위 테스트 " + travelList.size());
-//					while(!travelList.isEmpty()) {
-//						int[] t = travelList.poll();
-// 						System.out.println(t[0] + " " + t[1]);
-//					}
-//					System.out.println(travelList.size() + " " + travelList.peek()[1]);
-//					list = newList;
 					
 					break;
 			}
@@ -229,5 +185,3 @@ public class Main {
 //		System.out.println("s: " + s + " dist: " + Arrays.toString(dist));
 	}
 }
-
-// 예외 케이스: 상품삭제(deleted:1)
