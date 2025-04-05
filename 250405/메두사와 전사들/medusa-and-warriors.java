1e58f25522b3599aecec010eb10ea12fe42854c2
gyu <78714820+MoonGyu1@users.noreply.github.com>
2025-04-05 21:42:48 +0900

    Title: 메두사와 전사들, Time: 195ms, Memory: 15MB, Status: Passed - Codetree

--- 250405/메두사와 전사들/medusa-and-warriors.java
+++ 250405/메두사와 전사들/medusa-and-warriors.java
@@ -1,323 +1,289 @@
 import java.util.*;
 import java.io.*;
 
+// 시간복잡도: O(N^4 * M)
 public class Main {
-    static int N, M;
-    static int SR, SC, ER, EC;
-    static ArrayList<Warrior> warriors;
-    static int[][] map;
-    static boolean[][] isShown;
+	static int N, M;
+	static int SR, SC, ER, EC;
+	static ArrayList<Warrior> warriors;
+	static int[][] map;
+	static boolean[][] isShown;
+	static int mCnt, sCnt, aCnt;
 
-    public static void main(String[] args) throws IOException {
-        // Runtime.getRuntime().gc();
-        // long usedMemory = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
-        // System.out.print(usedMemory + " bytes");
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
 
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
+		st = new StringTokenizer(br.readLine());
+		SR = Integer.parseInt(st.nextToken());
+		SC = Integer.parseInt(st.nextToken());
+		ER = Integer.parseInt(st.nextToken());
+		EC = Integer.parseInt(st.nextToken());
 
-        st = new StringTokenizer(br.readLine());
-        SR = Integer.parseInt(st.nextToken());
-        SC = Integer.parseInt(st.nextToken());
-        ER = Integer.parseInt(st.nextToken());
-        EC = Integer.parseInt(st.nextToken());
+		warriors = new ArrayList<>();
 
-        warriors = new ArrayList<>();
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < M; i++) {
+			warriors.add(new Warrior(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
+		}
 
-        st = new StringTokenizer(br.readLine());
-        for(int i = 0; i < M; i++) {
-            warriors.add(new Warrior(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
-        }
+		map = new int[N][N];
+		for(int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for(int j = 0; j < N; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
 
-        // System.out.println(N + " " + M);
-        // System.out.println(SR + " " + SC + " " + ER + " " + EC);
-        
-        map = new int[N][N];
-        for(int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
-            for(int j = 0; j < N; j++) {
-                map[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
+		// 1. 공원까지 최단 경로 구하기
+		ArrayList<int[]> route = getRoutes();
+		if(route.isEmpty()) {
+			System.out.println(-1); // 도달 불가한 경우 -1 출력 후 종료
+			return;
+		}
 
-        // usedMemory = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
-        // System.out.print(usedMemory + " bytes");
-        // System.out.println(Arrays.deepToString(map));
+		// 매 턴마다 진행
+		for(int[] r : route) {
+			// 메두사가 공원에 도착하면 0 출력 후 종료
+			if(r[0] == ER && r[1] == EC) {
+				System.out.println(0);
+				return;
+			}
 
-        // 0. 
-        ArrayList<int[]> route = getRoutes();
-        if(route.isEmpty()) {
-            System.out.println(-1);
-            return;
-        }
+			// 전사 이동 거리, 돌이 된 전사 수, 공격한 전사 수
+			mCnt = 0; sCnt = 0; aCnt = 0;
 
-        // usedMemory = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
-        // System.out.print(usedMemory + " bytes");
+			// 1. 메두사의 이동
+			SR = r[0]; SC = r[1]; // 한 칸 이동
 
-        for(int[] r : route) {
-            if(r[0] == ER && r[1] == EC) {
-                System.out.println(0);
-                return;
-            }
+			// 동일 칸 전사 사라짐
+			for(Warrior w : warriors) {
+				if(w.r == SR && w.c == SC) {
+					w.dead = true;
+				}
+			}
 
-            int mCnt = 0, sCnt = 0, aCnt = 0;
+			// 2. 메두사의 시선
+			isShown = new boolean[N][N];
+			int max = -1;
 
-            // 1.
-            SR = r[0]; SC = r[1];
+			// 상하좌우 바라볼 때, 최대 전사 수를 가지는 시야각 구하기
+			for(int d : new int[]{0, 4, 8, 12}) { // 상하좌우
+				boolean[][] tmpShown = new boolean[N][N];
 
-            for(Warrior w : warriors) {
-                if(w.r == SR && w.c == SC) {
-                    w.dead = true;
-                }
-            }
+				see(d, SR, SC, true, tmpShown); // 메두사의 시선
 
-            // usedMemory = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
-            // System.out.print(usedMemory + " bytes");
+				// 전사들에게 가려지는 칸 처리
+				for(Warrior w : warriors) {
+					if(w.dead) continue;
 
-            // 2.
-            isShown = new boolean[N][N];
-            int max = -1;
-            for(int d : new int[]{0, 4, 8, 12}) { // 상하좌우
-                boolean[][] tmpShown = new boolean[N][N];
-                see(d, SR, SC, true, tmpShown);
-                
-                // System.out.println(Arrays.deepToString(tmpShown));
+					if(tmpShown[w.r][w.c]) {
+						int wd; // 전사의 상대적 위치
 
-                for(Warrior w : warriors) {
-                    if(w.dead) continue;
+						if(d == 0) { // 메두사 - 상
+							wd = w.c < SC ? 1 : w.c == SC ? 2 : 3; // 좌상, 직상, 우상
+						} else if(d == 4) { // 메두사 - 하
+							wd = w.c < SC ? 5 : w.c == SC ? 6 : 7; // 좌하, 직하, 우하
+						} else if(d == 8) { // 메두사 - 좌
+							wd = w.r < SR ? 9 : w.r == SR ? 10 : 11; // 상좌, 직좌, 하좌
+						} else { // 메두사 - 우
+							wd = w.r < SR ? 13 : w.r == SR ? 14 : 15; // 상우, 직우, 하우
+						}
 
-                    if(tmpShown[w.r][w.c]) {
-                        int td = 0;
-                        if(d == 0) {
-                            if(w.c < SC) td = 1;
-                            else if(w.c == SC) td = 2;
-                            else td = 3;
-                        } else if(d == 4) {
-                            if(w.c < SC) td = 5;
-                            else if(w.c == SC) td = 6;
-                            else td = 7;
-                        } else if(d == 8) {
-                            if(w.r < SR) td = 9;
-                            else if(w.r == SR) td = 10;
-                            else td = 11;
-                        } else {
-                            if(w.r < SR) td = 13;
-                            else if(w.r == SR) td = 14;
-                            else td = 15;
-                        }
+						see(wd, w.r, w.c, false, tmpShown); // 전사의 시선
+					}
+				}
 
-                        // if(d == 4) System.out.println(td);
+				// 더 많은 전사를 볼 수 있으면 갱신
+				int shownCnt = getShownCnt(tmpShown);
+				if(shownCnt > max) {
+					isShown = tmpShown;
+					max = shownCnt;
+				}
+			}
 
-                        see(td, w.r, w.c, false, tmpShown);
-                    }
-                }
+			sCnt += max; // 돌이 된 전사 수 카운트
 
-                int stunned = getShownCnt(tmpShown);
-                if(stunned > max) {
-                    isShown = tmpShown;
-                    max = stunned;
-                }
-            }
+			// 3. 전사들의 이동
+			// 4. 전사의 공격
+			for(Warrior w : warriors) {
+				for(int i = 1; i <= 2; i++) {
+					if(w.dead || isShown[w.r][w.c]) continue; // 사라졌거나, 돌이 된 경우 패스
+					moveAttack(w, i);
+				}
+			}
 
-            sCnt += max;
-            // usedMemory = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
-            // System.out.print(usedMemory + " bytes");
-            // System.out.println(Arrays.deepToString(isShown));
+			// 5. 전사 이동 거리 합, 돌이 된 전사 수, 공격한 전사 수 출력
+			System.out.println(mCnt + " " + sCnt + " " + aCnt);
+		}
+	}
 
-            // 3.
-            for(Warrior w : warriors) {
-                // System.out.println("w: " + w.r + "," + w.c);
+	static class Warrior {
+		int r, c;
+		boolean dead = false;
 
-                if(w.dead || isShown[w.r][w.c]) continue;
+		Warrior(int r, int c) {
+			this.r = r;
+			this.c = c;
+		}
+	}
 
-                for(int[] d : new int[][]{{-1, 0}, {1, 0}, {0, -1}, {0, 1}}) { // 상하좌우
-                    int x = w.r, y = w.c;
-                    int nx = x + d[0], ny = y + d[1];
+	static ArrayList<int[]> getRoutes() {
+		class Node {
+			int x, y;
+			String route;
 
-                    if(inRange(nx, ny) && !isShown[nx][ny] && getDist(nx, ny, SR, SC) < getDist(x, y, SR, SC)) {
-                        w.r = nx; w.c = ny;
-                        mCnt++;
+			Node(int x, int y, String route) {
+				this.x = x;
+				this.y = y;
+				this.route = route;
+			}
+		}
 
-                        // System.out.println(w.r + "," + w.c);
-                        // 4.
-                        if(w.r == SR && w.c == SC) {
-                            w.dead = true;
-                            aCnt++;
-                        }
-                        break;
-                    }
-                }
+		Queue<Node> q = new ArrayDeque<>();
+		boolean[][] visited = new boolean[N][N];
 
-                if(w.dead) continue;
+		q.add(new Node(SR, SC, ""));
+		visited[SR][SC] = true;
 
-                for(int[] d : new int[][]{{0, -1}, {0, 1}, {-1, 0}, {1, 0}}) { // 좌우상하
-                    int x = w.r, y = w.c;
-                    int nx = x + d[0], ny = y + d[1];
+		while(!q.isEmpty()) {
+			Node node = q.poll();
+			int x = node.x, y = node.y;
+			String route = node.route;
 
-                    if(inRange(nx, ny) && !isShown[nx][ny] && getDist(nx, ny, SR, SC) < getDist(x, y, SR, SC)) {
-                        w.r = nx; w.c = ny;
-                        mCnt++;
+			if(x == ER && y == EC) {
+				String[] strs = route.split("/");
+				ArrayList<int[]> result = new ArrayList<>();
 
-                        // System.out.println(w.r + "," + w.c);
-                        // 4.
-                        if(w.r == SR && w.c == SC) {
-                            w.dead = true;
-                            aCnt++;
-                        }
-                        break;
-                    }
-                }
-            }
+				for(String str : strs) {
+					if(str.isEmpty()) continue; // 주의: 맨 앞 빈 스트링 예외처리
 
-            // usedMemory = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
-            // System.out.print(usedMemory + " bytes");
+					String[] pos = str.split(",");
+					result.add(new int[]{Integer.parseInt(pos[0]), Integer.parseInt(pos[1])});
+				}
+				return result;
+			}
 
-            // 5.
-            System.out.println(mCnt + " " + sCnt + " " + aCnt);
+			// 상하좌우
+			for(int[] d : new int[][]{{-1, 0}, {1, 0}, {0, -1}, {0, 1}}) {
+				int nx = x + d[0], ny = y + d[1];
+				if(inRange(nx, ny) && map[nx][ny] == 0 && !visited[nx][ny]) {
+					q.add(new Node(nx, ny, route + "/" + nx + "," + ny));
+					visited[nx][ny] = true; // 주의: BFS 방문 처리 안 하면 메모리 초과 (무한 루프)
+				}
+			}
+		}
 
-            // break;
-        }
-    }
+		return new ArrayList<>();
+	}
 
-    // dir = 0 - 1,2,3, 4 - 5,6,7, 8 - 9,10,11, 12 - 13,14,15
-    // 상 - 좌상,상,우상, 하 - 좌하,하,우하, 좌 - 상좌,좌,하좌, 우 - 상우,우,하우
-    static void see(int dir, int r, int c, boolean flag, boolean[][] shown) {
-        if(dir == 0 || dir == 1 || dir == 2 || dir == 3) {
-            int fromy = Math.max(0, (dir == 0 || dir == 1) ? c-1 : c);
-            int toy = Math.min((dir == 0 || dir == 3) ? c+1 : c, N-1);
+	// [dir]
+	// 상(0) - 좌상(1), 직상(2), 우상(3)
+	// 하(4) - 좌하(5), 직하(6), 우하(7)
+	// 좌(8) - 상좌(9), 직좌(10), 하좌(11)
+	// 우(12) - 상우(13), 직우(14), 하우(15)
 
-            for(int x = r-1; x >= 0; x--) {
-                for(int y = fromy; y <= toy; y++) {
-                    shown[x][y] = flag;
-                }
-                if(dir == 0 || dir == 1) fromy = Math.max(0, fromy -1);
-                if(dir == 0 || dir == 3) toy = Math.min(toy + 1, N-1);
-            }
-        } else if(dir == 4 || dir == 5 || dir == 6 || dir == 7) {
-            int fromy = Math.max(0, (dir == 4 || dir == 5) ? c-1 : c);
-            int toy = Math.min(N-1, (dir == 4 || dir == 7) ? c+1 : c);
+	// [flag]
+	// 메두사 - true, 전사 - false
+	static void see(int dir, int r, int c, boolean flag, boolean[][] shown) {
+		// 메두사 - 상
+		if(dir == 0 || dir == 1 || dir == 2 || dir == 3) {
+			int fromy = Math.max(0, (dir == 0 || dir == 1) ? c-1 : c); // 주의: 범위 바운드 처리 필요
+			int toy = Math.min(N-1, (dir == 0 || dir == 3) ? c+1 : c);
 
-            for(int x = r+1; x < N; x++) {
-                for(int y = fromy; y <= toy; y++) {
-                    shown[x][y] = flag;
-                }
-                if(dir == 4 || dir == 5) fromy = Math.max(0, fromy -1);
-                if(dir == 4 || dir == 7) toy = Math.min(toy + 1, N-1);
-            }
-        }
-        // 좌 - 상좌,좌,하좌 
-        else if(dir == 8 || dir == 9 || dir == 10 || dir == 11) {
-            int fromx = Math.max(0, (dir == 8 || dir == 9) ? r-1 : r);
-            int tox = Math.min(N-1, (dir == 8 || dir == 11) ? r+1 : r);
+			for(int x = r - 1; x >= 0; x--) {
+				for(int y = fromy; y <= toy; y++) {
+					shown[x][y] = flag;
+				}
+				if(dir == 0 || dir == 1) fromy = Math.max(0, fromy -1);
+				if(dir == 0 || dir == 3) toy = Math.min(N-1, toy + 1);
+			}
+		}
+		// 메두사 - 하
+		else if(dir == 4 || dir == 5 || dir == 6 || dir == 7) {
+			int fromy = Math.max(0, (dir == 4 || dir == 5) ? c-1 : c);
+			int toy = Math.min(N-1, (dir == 4 || dir == 7) ? c+1 : c);
 
-            for(int y = c-1; y >= 0; y--) {
-                for(int x = fromx; x <= tox; x++) {
-                    shown[x][y] = flag;
-                }
-                if(dir == 8 || dir == 9) fromx = Math.max(0, fromx -1);
-                if(dir == 8 || dir == 11) tox = Math.min(N-1, tox + 1);
-            }
-        }
-        // 우 - 상우,우,하우
-        else if(dir == 12 || dir == 13 || dir == 14 || dir == 15) {
-            int fromx = Math.max(0, (dir == 12 || dir == 13) ? r-1 : r);
-            int tox = Math.min(N-1, (dir == 12 || dir == 15) ? r+1 : r);
+			for(int x = r + 1; x < N; x++) {
+				for(int y = fromy; y <= toy; y++) {
+					shown[x][y] = flag;
+				}
+				if(dir == 4 || dir == 5) fromy = Math.max(0, fromy -1);
+				if(dir == 4 || dir == 7) toy = Math.min(N-1, toy + 1);
+			}
+		}
+		// 메두사 - 좌
+		else if(dir == 8 || dir == 9 || dir == 10 || dir == 11) {
+			int fromx = Math.max(0, (dir == 8 || dir == 9) ? r-1 : r);
+			int tox = Math.min(N-1, (dir == 8 || dir == 11) ? r+1 : r);
 
-            for(int y = c+1; y < N; y++) {
-                for(int x = fromx; x <= tox; x++) {
-                    shown[x][y] = flag;
-                }
-                if(dir == 12 || dir == 13) fromx = Math.max(0, fromx -1);
-                if(dir == 12 || dir == 15) tox = Math.min(N-1, tox + 1);
-            }
-        }
-    }
+			for(int y = c - 1; y >= 0; y--) {
+				for(int x = fromx; x <= tox; x++) {
+					shown[x][y] = flag;
+				}
+				if(dir == 8 || dir == 9) fromx = Math.max(0, fromx -1);
+				if(dir == 8 || dir == 11) tox = Math.min(N-1, tox + 1);
+			}
+		}
+		// 메두사 - 우
+		else if(dir == 12 || dir == 13 || dir == 14 || dir == 15) {
+			int fromx = Math.max(0, (dir == 12 || dir == 13) ? r-1 : r);
+			int tox = Math.min(N-1, (dir == 12 || dir == 15) ? r+1 : r);
 
-    static int getShownCnt(boolean[][] shown) {
-        int cnt = 0;
+			for(int y = c + 1; y < N; y++) {
+				for(int x = fromx; x <= tox; x++) {
+					shown[x][y] = flag;
+				}
+				if(dir == 12 || dir == 13) fromx = Math.max(0, fromx -1);
+				if(dir == 12 || dir == 15) tox = Math.min(N-1, tox + 1);
+			}
+		}
+	}
 
-        for(Warrior w : warriors) {
-            if(!w.dead && shown[w.r][w.c]) {
-                cnt++;
-            }
-        }
+	static int getShownCnt(boolean[][] shown) {
+		int cnt = 0;
 
-        return cnt;
-    }
+		for(Warrior w : warriors) {
+			if(!w.dead && shown[w.r][w.c]) {
+				cnt++;
+			}
+		}
 
-    static class Warrior {
-        int r, c;
-        boolean dead = false;
+		return cnt;
+	}
 
-        Warrior(int r, int c) {
-            this.r = r;
-            this.c = c;
-        }
-    }
+	static void moveAttack(Warrior w, int step) {
+		int[][] dist = step == 1 ? new int[][]{{-1, 0}, {1, 0}, {0, -1}, {0, 1}} // 상하좌우
+			: new int[][]{{0, -1}, {0, 1}, {-1, 0}, {1, 0}}; // 좌우상하
 
-    static ArrayList<int[]> getRoutes() {
-        class Node {
-            int x, y;
-            String route;
+		for(int[] d : dist) {
+			int x = w.r, y = w.c;
+			int nx = x + d[0], ny = y + d[1];
 
-            Node(int x, int y, String route) {
-                this.x = x;
-                this.y = y;
-                this.route = route;
-            }
-        }
+			// 격자 내 O, 메두사 시야 X, 거리 감소 O이면 한 칸 이동
+			if(inRange(nx, ny) && !isShown[nx][ny] && getDist(nx, ny, SR, SC) < getDist(x, y, SR, SC)) {
+				w.r = nx; w.c = ny;
+				mCnt++; // 전사 이동 수 증가
 
-        Queue<Node> q = new ArrayDeque<>();
-        boolean[][] visited = new boolean[N][N];
+				// 4. 전사의 공격
+				if(w.r == SR && w.c == SC) { // 메두사와 같은 칸에 도달한 경우
+					w.dead = true;
+					aCnt++; // 공격한 전사 수 카운트
+				}
 
-        q.add(new Node(SR, SC, ""));
-        visited[SR][SC] = true;
+				return;
+			}
+		}
+	}
 
-        // Runtime.getRuntime().gc();
-        // long usedMemory = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
-        // System.out.print(usedMemory + " bytes(bfs)");
+	static boolean inRange(int x, int y) {
+		return 0 <= x && x < N && 0 <= y && y < N;
+	}
 
-        while(!q.isEmpty()) {
-            Node node = q.poll();
-            int x = node.x, y = node.y;
-            String route = node.route;
-
-            if(x == ER && y == EC) {
-                // usedMemory = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
-                // System.out.print(usedMemory + " bytes(bfs end)");
-
-                String[] tmp = route.split("/");
-                ArrayList<int[]> result = new ArrayList<>();
-
-                for(String t : tmp) {
-                    if(t.isEmpty()) continue;
-                    String[] tmp2 = t.split(",");
-                    result.add(new int[]{Integer.parseInt(tmp2[0]), Integer.parseInt(tmp2[1])});
-                }
-                return result;
-            }
-
-            // 상하좌우
-            for(int[] dist : new int[][]{{-1, 0}, {1, 0}, {0, -1}, {0, 1}}) {
-                int nx = x + dist[0], ny = y + dist[1];
-                if(inRange(nx, ny) && map[nx][ny] == 0 && !visited[nx][ny]) {
-                    q.add(new Node(nx, ny, route + "/" + nx + "," + ny));
-                    visited[nx][ny] = true;
-                }
-            }
-        }
-
-        return new ArrayList<>();
-    }
-
-    static boolean inRange(int x, int y) {
-        return 0 <= x && x < N && 0 <= y && y < N;
-    }
-
-    static int getDist(int x1, int y1, int x2, int y2) {
-        return Math.abs(x1-x2) + Math.abs(y1-y2);
-    }
+	static int getDist(int x1, int y1, int x2, int y2) {
+		return Math.abs(x1 - x2) + Math.abs(y1 - y2);
+	}
 }
